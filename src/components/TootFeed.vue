5e7d26252c38a2078d2a7d64547102accb951255
Bill <bill.chandos@gmail.com>
2020-06-12 07:53:13 -0700

    exp w/ hover and click capture

--- src/components/TootFeed.vue
+++ src/components/TootFeed.vue
@@ -1,16 +1,28 @@
 <template>
 <!-- TODO: Figure out desired auto-scroll behavior (maybe to top when clicking menu, no change during regular updates) -->
     <v-col class="shrink">
-        <v-card width="500px" v-for="toot in englishToots" :key="toot.id" class="grey darken-3 pa-2 ma-2">
-            <v-card-title class="headline">
-            <v-img :src="toot.account.avatar" class="avatar ma-2" max-height="2rem" max-width="2rem"/>
-            <span v-html="toot.account.display_name || toot.account.username"/>
-            </v-card-title>
+        <v-card dark width="500px" v-for="toot in filteredToots" :key="toot.id" class="grey darken-3 pa-2 ma-2">
+            <!-- TODO: Some kind of slick user info interface -->
+            <!-- <v-hover v-slot:default="{ hover }"> -->
+              <v-card-title class="headline">
+                <v-img :src="toot.account.avatar" class="avatar ma-2" max-height="2rem" max-width="2rem"/>
+                <span v-html="toot.account.display_name || toot.account.username"/>
+                <!-- <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out darken-2 v-card--reveal caption white--text"
+                    style="height: 100%;"
+                    v-html="toot.account.note"
+                  >
+                  </div>
+                </v-expand-transition> -->
+              </v-card-title>
+            <!-- </v-hover> -->
             <v-card-subtitle class="overline pl-7">
             {{ toot.created_at | formatDate }}
             </v-card-subtitle>
             <v-card-text v-if="toot.new_content" v-html="toot.new_content"></v-card-text>
-            <v-card-text v-else v-html="toot.content"></v-card-text>
+            <v-card-text v-else v-html="toot.content" @click.prevent="captureClick"></v-card-text>
             <v-img v-for="image in toot.media_attachments" :key="image.id" :src="image.preview_url" :alt="image.description"></v-img>
         </v-card>
     </v-col>
@@ -24,7 +36,7 @@ export default {
       sharedState: store.state,
     }),
     computed: {
-      englishToots: function() {
+      filteredToots: function() {
         return this.sharedState.currentToots.filter( (t) => {
           let combinedArray = [true];
           if (this.sharedState.activeFilters.includes('english')) {
@@ -44,6 +56,13 @@ export default {
       formatDate: function(date) {
         return new Date(date).toLocaleString('en');
       },
-    }
+    },
+
+    methods: {
+      // TODO: capture tag clicks, pass all others
+      captureClick: function(ev) {
+        console.log(ev);
+      }
+    },
 }
 </script>
