80a715868a416f27d6404dc97b2fa67855fbae15
Bill <bill.chandos@gmail.com>
2020-06-13 14:33:39 -0700

    trying to add account into

---
+++ src/components/AccountInfo.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  @{{ account.username }}
+  {{ account.followers_count }} Followers / {{ account.following_count }} Following / {{ account.statuses_count }} Posts
+</div>
+</template>
+
+<script>
+import { store } from "../store.js";
+
+export default {
+    data: () => ({
+      sharedState: store.state,
+    }),
+    props: {
+        account: Object,
+    }
+}
+</script>
