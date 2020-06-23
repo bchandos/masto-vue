db9f6ed2d5fbde2fde0f8a9b14c71d745116f734
Bill <bill.chandos@gmail.com>
2020-06-22 20:21:21 -0700

    Filter component

---
+++ src/components/FilterList.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-list dense>
+        <v-list-item-group multiple v-model="userState.activeFilters">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                FILTERS
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item value="english">
+            <v-list-item-action>
+              <v-icon>mdi-filter</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                English
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item value="sensitive">
+            <v-list-item-action>
+              <v-icon>mdi-filter</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                Sensitive
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item value="bots">
+            <v-list-item-action>
+              <v-icon>mdi-filter</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                Bots
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+</template>
+
+<script>
+import { store } from "../store.js";
+
+export default {
+    data: () => ({
+      userState: store.state.userState,
+      appState: store.state.appState,
+      settings: store.state.settings,
+    }),
+}
+</script>
--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -105,53 +105,17 @@
         </v-list-item-group>
       </v-list>
       <v-spacer />
-      <v-list dense>
-        <v-list-item-group multiple v-model="userState.activeFilters">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                FILTERS
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item value="english">
-            <v-list-item-action>
-              <v-icon>mdi-filter</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                English
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item value="sensitive">
-            <v-list-item-action>
-              <v-icon>mdi-filter</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                Sensitive
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item value="bots">
-            <v-list-item-action>
-              <v-icon>mdi-filter</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                Bots
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
+      <FilterVue />
     </v-navigation-drawer>
 </template>
 <script>
 import { store } from "../store.js";
+import FilterVue from '../components/FilterList.vue';
 
 export default {
+    components: {
+      FilterVue,
+    },
     data: () => ({
       userState: store.state.userState,
       appState: store.state.appState,
