97b5cc31f74673f3942258ac5f6ab21fb02ec4a4
Bill <bill.chandos@gmail.com>
2020-08-29 09:54:42 -0700

    try to fix workbox plugin for deploy

--- vue.config.js
+++ vue.config.js
@@ -26,11 +26,11 @@ module.exports = {
     manifestOptions: {},
 
     // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
   }
 }
